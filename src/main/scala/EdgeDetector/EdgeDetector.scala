6a1657c1a83252fd90bd0b00aef3ff38611ee94c
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2022-10-27 14:25:32 -0700

    edge detector parmeterization

--- src/main/scala/EdgeDetector/EdgeDetector.scala
+++ src/main/scala/EdgeDetector/EdgeDetector.scala
@@ -2,12 +2,26 @@ package EdgeDetector
 
 import chisel3._
 
-class EdgeDetector extends Module {
+class EdgeDetector(width: Int = 1, bothEdges: Boolean = false) extends Module {
   val io = IO(new Bundle {
-    val in = Input(Bool())
-    val out = Output(Bool())
+    val in = Input(Vec(Bool(), width))
+    val out = Output(Vec(Bool(), width))
   })
-  val register=Reg(Bool())
+
+  if (!bothEdges) {
+    val history = Reg(Bool())
+    history := io.in
+
+    when (!history && io.in) {
+      io.out := true.B
+    }.otherwise {
+      io.out := false.B
+    }
+  } else {
+    // TODO: modify this such that when bothEdges is true the output should be a 1 cycle wide pulse when the input has a rising and falling edge
+
+  }
+
   when (io.in =/= register) {
     register:=true.B
   }
--- src/test/scala/EdgeDetector/EdgeDetectorSpec.scala
+++ src/test/scala/EdgeDetector/EdgeDetectorSpec.scala
@@ -7,7 +7,16 @@ import chisel3._
 
 class EdgeDetectorSpec extends AnyFreeSpec with ChiselScalatestTester {
   "edge detector should detect rising edges" in {
-    test(new EdgeDetector()) { c =>
+    test(new EdgeDetector(false)).withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) { c =>
+      c.io.in.poke(0.U)
+      c.clock.step(1)
+      c.io.out.expect(0.U)
+      c.io.in.poke(1.U)
+      c.clock.step(1)
+      c.io.out.expect(1.U)
+      c.clock.step(1)
+      c.io.out.expect(0.U)
+
       c.io.in.poke(0.U)
       c.clock.step(1)
       c.io.out.expect(0.U)
@@ -18,4 +27,5 @@ class EdgeDetectorSpec extends AnyFreeSpec with ChiselScalatestTester {
       c.io.out.expect(0.U)
     }
   }
+  // TODO: new test case for the falling / rising edge case
 }
